44d90b5e054eb24dd761fd777619969ae48fdbb7
trbianca <trbianca@outlook.com>
2022-01-20 15:56:01 -0300

    MOD2 -  Desafio 03 For While

---
+++ spript.js
@@ -0,0 +1,45 @@
+//1 - Criar um loop que conte de 1 até 10 usando FOR
+
+// for(let cont= 1; cont <=10 ; cont++){
+//     console.log(cont);
+// }
+
+//2 - Criar um loop que conte de 10 até 1 usando WHILE
+ 
+// let cont = 1;
+
+// while( cont <= 10){
+//     console.log(cont);
+//     cont = cont + 1;
+// }
+
+//3 - Criar um loop que conte todos os números ímpares de 1 até 100
+
+
+//FOR
+// for(let cont= 1; cont <=100; cont+=2){
+//     console.log(cont);
+// }
+
+//WHILE
+// let cont = 1;
+// while(cont <=100){
+// console.log(cont);
+// cont = cont + 2;
+// }
+
+
+//4- Criar um loop que conte todos os números pares de 0 a 100
+
+
+//FOR
+// for(let cont = 2; cont <= 100; cont+=2){
+//     console.log(cont);
+// }
+
+//WHILE
+// let cont = 2;
+// while(cont <= 100){
+// console.log(cont);
+// cont = cont + 2;
+// }
